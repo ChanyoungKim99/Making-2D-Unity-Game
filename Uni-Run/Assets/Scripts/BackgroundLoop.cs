d6d7b423ca962f263384caa436ee1a483357c07b
ChanyoungKim99 <davidkjcy@gmail.com>
2023-05-01 18:58:13 +0900

    Making Game Manager

--- Uni-Run/Assets/Scripts/BackgroundLoop.cs
+++ Uni-Run/Assets/Scripts/BackgroundLoop.cs
@@ -4,16 +4,31 @@
 public class BackgroundLoop : MonoBehaviour {
     private float width; // 배경의 가로 길이
 
-    private void Awake() {
-        // 가로 길이를 측정하는 처리
+    private void Awake()            // Start 메서드처럼 1회 자동실행되는 이벤트 메서드, 단 Start보다 1프레임 빠름
+    {
+        // 배경의 가로길이를 구하는 처리
+        // BoxCollider2D 컴포넌트의 Size 필드의 x 값을 가로 길이로 사용
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
+
+        // BoxCollider2D 컴포넌트가 스프라이트에 맞춰 크기를 자동으로 설정해준다
     }
 
-    private void Update() {
-        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+    private void Update() 
+    {
+        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 재배치
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
     }
 
     // 위치를 리셋하는 메서드
-    private void Reposition() {
-        
+    private void Reposition() 
+    {
+        // 현재 위치에서 오른쪽으로 가로 길이 * 2만큼 이동
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2) transform.position + offset;
+        // transform.position은 Vector3 타입이기 떄문에 Vector2로 명시적 형변환
     }
 }
--- Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -5,6 +5,9 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
-        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        // 초당 speed의 속도로 왼쪽으로 평행이동
+        transform.Translate(Vector3.left * speed * Time.deltaTime);     // Translate 메서드 = 평행이동 메서드
+        // Vector3.left = (-1,0,0)인 방향벡터
+ 
     }
 }
